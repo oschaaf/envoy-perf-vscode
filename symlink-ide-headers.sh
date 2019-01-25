51fef290a5c37aabf8164ee4116fc878c3067b96
Otto van der Schaaf <oschaaf@we-amp.com>
2019-01-25 11:53:46 +0100

    Add script to symlink IDE headers, update .gitignore
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

---
+++ symlink-ide-headers.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# This script symlinks bazel's output base into the root of the workspace
+# as a directory '/tmp/nighthawk_bazel_output_base'. This can be used by IDE's to
+# get to the header files of dependencies.
+# See .vscode/c_cpp_properties.json for an example.
+
+set -e
+
+if [ -f envoy-perf.code-workspace ]; then
+    pushd envoy-perf/nighthawk
+    target=$(bazel info output_base)
+    popd
+    ln -sf "$target" /tmp/nighthawk_bazel_output_base
+    echo "Symlinked $target to /tmp/nighthawk_bazel_output_base"
+else
+    echo "This script should be executed from the git root"
+    exit 1
+fi
