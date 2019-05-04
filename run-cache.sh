afd5d30d83acdddd11e6d0d4cee40114e94dc3cb
Otto van der Schaaf <oschaaf@we-amp.com>
2019-05-05 01:08:11 +0200

    150GB cache
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- run-cache.sh
+++ run-cache.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 go get github.com/buchgr/bazel-remote
-go run github.com/buchgr/bazel-remote --dir ${HOME}/bazel_cache --host 127.0.0.1 --port 28080 --max_size 64
+go run github.com/buchgr/bazel-remote --dir ${HOME}/bazel_cache --host 127.0.0.1 --port 28080 --max_size 150
